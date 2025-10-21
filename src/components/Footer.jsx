9e0491b2f0d9df779e67000054d475ef27f20357
TaimoorAli3921 <alit8029@gmail.com>
2025-10-21 11:44:51 +0500

    Added footer with Github link

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Footer() {
+ 
+
+  return (
+    <footer className="footer">
+  <p>
+    View source on{" "}
+    <a 
+      href="https://github.com/TaimoorAli25jsf2002/propsdemo" 
+      target="_blank" 
+      rel="noopener noreferrer"
+    >
+      GitHub
+    </a>
+  </p>
+</footer>
+
+  );
+}
